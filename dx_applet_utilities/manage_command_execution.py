ddb5e75a9d246a46fa39dee1460abd6bd6ffbd84
Michael Gonzalez <MikeGonzalez2015@gmail.com>
2016-03-16 16:44:40 -0400

    Function to execute commands

--- dx_applet_utilities/manage_command_execution.py
+++ dx_applet_utilities/manage_command_execution.py
@@ -2,9 +2,43 @@ import os
 import sys
 import dxpy
 import logging
+import subprocess
 
 
 logger = logging.getLogger(__name__)
 logger.addHandler(dxpy.DXLogHandler())
 logger.propagate = False
 logger.setLevel('INFO')
+
+
+def execute_command(command, debug=False):
+
+    """Executes given command
+
+    :param: `command`: Command to execute
+    :param: `debug`: If True, will log all messages to console
+    :returns: Object with results of the executed command
+    """
+
+    if debug:
+        p = subprocess.Popen(command, shell=True)
+        p.wait()
+        out = ""
+        err = ""
+        sys_code = p.returncode
+    else:
+        p = subprocess.Popen(
+            command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        out,err = p.communicate()
+        sys_code = p.returncode
+
+    return {
+            "sysCode": sys_code,
+            "cmd": command,
+            "out": out.strip(),
+            "err": err.strip()
+    }
