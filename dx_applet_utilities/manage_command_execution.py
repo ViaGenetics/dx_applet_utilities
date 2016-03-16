2a8bf0c2cfb1d4c33c8c5fb801e13e014d16dbd1
Michael Gonzalez <MikeGonzalez2015@gmail.com>
2016-03-16 16:41:03 -0400

    Add packages to import and logging

---
+++ dx_applet_utilities/manage_command_execution.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import dxpy
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.addHandler(dxpy.DXLogHandler())
+logger.propagate = False
+logger.setLevel('INFO')
