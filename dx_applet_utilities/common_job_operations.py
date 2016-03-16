b47f344457ca47f48b413746a927a68e3b3c1356
Michael Gonzalez <MikeGonzalez2015@gmail.com>
2016-03-16 17:05:41 -0400

    Function to serialize file data into JSON object

--- dx_applet_utilities/common_job_operations.py
+++ dx_applet_utilities/common_job_operations.py
@@ -2,9 +2,25 @@ import os
 import sys
 import dxpy
 import logging
+import json
 
 
 logger = logging.getLogger(__name__)
 logger.addHandler(dxpy.DXLogHandler())
 logger.propagate = False
 logger.setLevel('INFO')
+
+
+def load_json_from_file(path_to_file):
+
+    """Load JSON file from a file
+
+    :param: `path_to_file`: Full path to a file to convert to JSON object
+    :returns: The input file as a serialized JSON object
+    """
+
+    json_object = {}
+    with open(path_to_file, 'r') as file_data:
+        json_object = json.load(file_data)
+
+    return json_object
