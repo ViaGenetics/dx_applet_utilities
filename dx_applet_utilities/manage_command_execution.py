e411cb784dd3253012c2595a90a3525493baffe2
Michael Gonzalez <MikeGonzalez2015@gmail.com>
2016-03-16 16:48:51 -0400

    Function to verify commands status

--- dx_applet_utilities/manage_command_execution.py
+++ dx_applet_utilities/manage_command_execution.py
@@ -42,3 +42,28 @@ def execute_command(command, debug=False):
             "out": out.strip(),
             "err": err.strip()
     }
+
+
+def check_execution_syscode(command_result, command_description):
+
+    """Verifies if executed command finished successfully
+
+    :param: `command_result`: Object returned from execute_command function
+    :param: `command_description`: Description of command for display in logs
+    """
+
+    if command_result["sysCode"] == 0:
+        logger.info('{0}: Execution of {1} finished successfully!'.format(
+            strftime("%Y-%m-%d %H:%M:%S", gmtime()), command_description)
+        )
+
+    else:
+        logger.error('{0}: Execution of {1} failed!'.format(
+            strftime("%Y-%m-%d %H:%M:%S", gmtime()), command_description)
+        )
+        logger.error('{0}: Command that was executed: {1}'.format(
+            strftime("%Y-%m-%d %H:%M:%S", gmtime()), command_result["cmd"])
+        )
+        logger.error('STDOUT: {0}'.format(command_result["out"]))
+        logger.error('STDERR: {0}'.format(command_result["err"]))
+        sys.exit(1)
