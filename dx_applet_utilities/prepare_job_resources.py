e4f01d8b4eb800af26204739dfd74973150e282e
Michael Gonzalez <MikeGonzalez2015@gmail.com>
2016-03-16 16:59:00 -0400

    Function to calculate max RAM to allocate to processes

--- dx_applet_utilities/prepare_job_resources.py
+++ dx_applet_utilities/prepare_job_resources.py
@@ -3,9 +3,33 @@ import sys
 import dxpy
 import logging
 import math
+from dx_applet_utilities.manage_command_execution import execute_command, check_execution_syscode
 
 
 logger = logging.getLogger(__name__)
 logger.addHandler(dxpy.DXLogHandler())
 logger.propagate = False
 logger.setLevel('INFO')
+
+
+def max_memory(percent_to_utilize):
+
+    """Return the max RAM to be used by applet
+
+    :param: `percent_to_utilize`: Percent of RAM to allocate for processes
+    :returns: The amount of RAM in MB to allocate to a given process
+    """
+
+    total_ram_cmd = "head -n1 /proc/meminfo"
+    calculate_ram_cmd = "awk '\{ print int($2*{0}/1024) \}'".format(percent_to_utilize)
+    ram_allocation = execute_command("{0} | {1}".format(
+        total_ram_cmd, calculate_ram_cmd)
+    )
+
+    if ram_allocation["sysCode"] == 0:
+        return int(ram_allocation["out"])
+    else:
+        logger.warning("{0}: Unable to calculate RAM usage! Setting to default".format(
+            strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+        )
+        return 512
